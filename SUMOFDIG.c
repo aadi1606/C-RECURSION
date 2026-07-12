c1243446eac7031f92a3bbb3fffe23a88558bb7c
aadi1606 <aadityashakya2008@gmail.com>
2026-07-12 18:42:22 +0530

    SUM OF DIGITS OF A NUMBER

---
+++ SUMOFDIG.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int sum(int number){
+    if(number<10)
+      return number;
+    return (number % 10) + sum(number / 10);
+}
+int main(){
+    int n;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    printf("SUM OF DIGITS OF %d : %d",n,sum(n));
+    return 0;
+}
