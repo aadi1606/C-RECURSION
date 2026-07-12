816fb87a81f0be368f18cfec586ae9cea8a7539f
aadi1606 <aadityashakya2008@gmail.com>
2026-07-12 18:36:27 +0530

    FIBONACCI SERIES

---
+++ FIBONACCI.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int fibbonaci(int n){
+    if(n<=0)
+      return 0;
+    else if(n==1)
+      return 1;
+    else{
+        return fibbonaci(n-1) + fibbonaci(n-2);
+    }
+}
+int main(){
+    int n;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    printf("ELEMENT AT INDEX %d : %d",n,fibbonaci(n));
+    return 0;
+}
